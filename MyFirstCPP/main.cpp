9644f6bcae6c9568c06dd7f6b5884e1b5c6c1628
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-01-23 12:31:17 -0700

    Staring my first app in C++ and added basic code to put stuff on the screen.

---
+++ MyFirstCPP/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  MyFirstCPP
+//
+//  Created by Nielsen, Ethan on 1/23/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#include <iostream> // *Pre proccesor directives have no semi colins.
+
+using namespace std; // For this entire file it uses std, so havingthis in methods is not needed.
+
+int main()
+{
+    // insert code here...
+    cout << "Hello, World!" << endl; // endl is basically a "new line" instead of doing "/n"
+    string input;
+    getline(cin, input);
+    cout << "You said:" << input << endl;
+    return 99;
+}
