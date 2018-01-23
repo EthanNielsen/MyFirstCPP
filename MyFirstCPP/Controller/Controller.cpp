e6bfec83466f815938e525f554678f2019e6eb49
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-01-23 12:44:48 -0700

    created the Controller.cpp and the Controller header. We created the start method int the Control header.

---
+++ MyFirstCPP/Controller/Controller.cpp
@@ -0,0 +1,9 @@
+//
+//  Controller.cpp
+//  MyFirstCPP
+//
+//  Created by Nielsen, Ethan on 1/23/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#include "Controller.hpp"
---
+++ MyFirstCPP/Controller/Controller.hpp
@@ -0,0 +1,21 @@
+//
+//  Controller.hpp
+//  MyFirstCPP
+//
+//  Created by Nielsen, Ethan on 1/23/18.
+//  Copyright © 2018 Ctech. All rights reserved.
+//
+
+#ifndef Controller_hpp
+#define Controller_hpp
+
+#include <iostream> // Only needs to be declared once because it will link to everything else.
+
+class Controller
+{
+public:
+    Controller();
+    void start();
+};
+
+#endif /* Controller_hpp */
